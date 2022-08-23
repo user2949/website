fc2d02ddded0fc278249443c0bb2f171d8c49667
Darryl Alcander Lim <103663597+user2949@users.noreply.github.com>
2022-08-23 17:25:04 +0700

    Add files via upload

---
+++ ERR403-1.js
@@ -0,0 +1,80 @@
+statement = if()
+{
+	{
+		localObject = ()
+		Warning = (Connectivity.Object = response)
+	}
+		GeneratePassword = (index.js = 90)	
+}
+	OpenLargeSize = 25
+		}
+			Math.rand&&Next.Double = 25
+			Math.rand&Next.First = 15
+			Math.Object.Max = 25
+				Math.ObjectRenderer = 180
+					}
+			Math.rand&&&Next.TRRS = 1780
+				}
+					Object.RendererSize = "5"
+					{
+					Object.PasswordGenerator = "ABCDEFGHIJKLMNOPQRSTUVYXZ"
+					Object.PasswordGeneratorLowercase = "abcdefghijklmnopqrstuvyxz"
+					Object.PasswordNumbers = "1234567890"
+					Object.PasswordCS = "~!@#$%^&*()_+-={}[]:;'<>/|\ "
+					}
+						Object if = (Handler.HTML = SS)
+							}
+								do }
+								return null;
+								
+								else }
+								return 0;
+								
+								else }
+								embed = Components.Data = file.cs = (__content__)
+								embed = Handler.ObjectImport()
+									}
+								
+								else }
+								Object.Kill()
+								boolean = Obj.Kill() = killTask
+									}
+								
+								else }
+								int = 25
+								nfd = 25
+								embed = 25
+								image = 25
+									}
+								dataRand.DoubleNext = 25
+									}
+									{
+										Object.NextDoubleValue = ()
+									}
+										Object.CMD = ()
+								else if = password('VczTKjca&9GU@E%v&+7-!d9Fq')
+								}
+								do } Allow.Object = krefix = (ImportSleep)
+									}
+									Object.Reminder()
+								if IP = '134.182.919.18' = true } else = blockConnection
+								if IP = '185.191.12.1' = true } else = ObjectUser = inObject of }
+								if IP = '255.255.255.255' = true } else = LogIn = localhost@root = (Password+GEN=258017927112)
+								
+									int = 2147483647;
+										else }
+									int = 9223372036854775807;
+										else }
+									int = 1.8492018370102793192E+181;
+										else }
+									int = 0;
+										else }
+									return 0;
+										else }
+									return invoke;
+										else }
+									return Object;
+										else }
+									return nullVector;
+								
+	}
