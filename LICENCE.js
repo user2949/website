ab0b69506a246b6b0f92d68bd296f4afa0090fc1
Darryl Alcander Lim <103663597+user2949@users.noreply.github.com>
2022-08-26 22:42:27 +0700

    Add files via upload

---
+++ LICENCE.js
@@ -0,0 +1,70 @@
+//Copyright Darryl Workstations (2007-2022)
+//Custom Licencing for Windows 10 Object
+
+//Any copies from this script then real-life actions would be taken.
+//Given to the rights to user2949 to publish on github.io
+//Un-Authorized use or copying of data without permissions is prohibited and rule 3 will be started,
+//M.I.T Licence 'https://opensource.org/licenses/MIT'
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import customjava
+import net.customjava.object.runobject.start
+import net.customjava.commands
+import net.customjava.importcommands
+import imagecustomjava
+import objects
+import xyzObject
+import int
+import stdio
+
+if = object() define(Object.ReturnObject = url;)
+	}
+	encoder.Languages {
+		Object = define(lit = 1)
+		Object = define(x)
+		Object = define(y)
+		Object = define(z)
+		{
+			{
+				index.Object = definedObject = get(clientvc)
+				{	
+					{
+						Math.int = '2147483647'
+						Math.int.Max = '0'
+					}
+						Margin = '0'
+						Padding = '0'
+						{
+							Object.DefineProperty = Margin.Math(int.Config = main)
+							{
+							main.int = PropertyObject.Return > ObjectConfig = }
+							
+							image 'https://localhost:39271/img/private/2u85oNm52mC6h3jTYCJn6k3y1ikMCk65u2KCm6j3/199781/291734/img.jpeg'
+							{
+								videoImagePlayback = 'videoplayback.mp4'
+							}
+								Object = html(Garbage.Collection = data.Object(SignReturning())
+								{
+									const width = 1024
+									const height = 768
+								}
+				Object.Define = Object.Width = Width = () = } > GetDisplayResolution = {
+					Object.Return = (Width > varAsync) syncBar = ()
+						const.Language = ('script.js')	
+						{
+							const width = widthProperty
+							const height = heightProperty
+						}
+				}
+				
+									var width = Object.Width(1024)
+									var height = Object.Height(768)
+								}
+							call.Object()
+							}
+							}
+						}
+					}
+			}
+		}
+	}
